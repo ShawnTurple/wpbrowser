7f13f469d0d4e8068747d41e045ef41ac9820726
Shawn Turple <shawn.turple@gov.bc.ca>
2019-08-19 12:02:05 -0700

    added phpdoc to entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,8 @@
 /repo/vendor/bin/phpcs -i
 #/repo/vendor/bin/phpcs -nps --colors --standard=Wordpress  --report-full=/project/.audit/phpcs-full.txt --report-summary=/project/.audit/phpcs-summary.txt /project
 /repo/vendor/bin/phpcs -nps --standard=/phpcs.xml --colors /project
+# Creates php docs from project folder, creates info in .phpdocs and excludes /vendor
+/repo/vendor/vin/phpdoc -d /project -t /project/.phpdocs -i /project/vendor/
 /bin/bash
 
 
