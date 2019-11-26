b82b3782174f3f0df970aead74e360bf14624bef
Shawn Turple <shawn.turple@gov.bc.ca>
2019-11-26 06:12:33 -0800

    Updates and Fixes
    * docker-entrypoint now has conditional to see what type of task is to be performed
    * tasks
    	* codecept
    	* phpcs
    	* phpcbf
    	* docs
    	* bash
    	* mysql
    	* composer
    * added mysql client

--- Dockerfile
+++ Dockerfile
@@ -6,13 +6,13 @@ ENV COMPOSER_ALLOW_SUPERUSER=1
 ENV PATH /repo:${PATH}
 
 # Install required system packages
-RUN 	apt-get update && apt-get -y install \
-	git zlib1g-dev libssl-dev libpng-dev  less vim graphviz libicu-dev zlib1g-dev libxslt1-dev\
+RUN apt-get update && apt-get -y install \
+	git zlib1g-dev libssl-dev libpng-dev  less vim graphviz libicu-dev zlib1g-dev libxslt1-dev mysql-client \
 	--no-install-recommends && apt-get clean && \
 	rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
 # Install php extensions
-RUN 	docker-php-ext-install bcmath zip gd pdo_mysql mysqli intl xsl; \
+RUN docker-php-ext-install bcmath zip gd pdo_mysql mysqli intl xsl; \
 	pecl install mongodb xdebug-2.6.0beta1 && \
 	docker-php-ext-enable mongodb.so xdebug ;
 
@@ -31,21 +31,24 @@ WORKDIR /repo
 ADD ./composer.json /repo/composer.json
 
 # Install vendor
-RUN 	composer install --prefer-dist --optimize-autoloader; \
+RUN composer install --prefer-dist --optimize-autoloader; \
 	ln -s /repo/vendor/bin/codecept /usr/local/bin/codecept; \
-	mkdir -p  /wordpress /project
+	mkdir -p  /wordpress /project /env
 
-RUN  sed -i -e "s/\.env/\.\.\/env\/\.env/" /repo/vendor/lucatume/wp-browser/codeception.dist.yml
+RUN sed -i -e "s/\.env/\.\.\/env\/\.env/" /repo/vendor/lucatume/wp-browser/codeception.dist.yml
 ADD docker-entrypoint.sh /usr/local/bin
 ADD phpcs.xml /
 ADD Wpbrowser.php /repo/vendor/lucatume/wp-browser/src/Codeception/Template/
+ADD ./.env /env
 
-RUN 	ln -s /repo/vendor/bin/phpcs /usr/local/bin/phpcs; \
+RUN ln -s /repo/vendor/bin/phpcs /usr/local/bin/phpcs; \
 	ln -s /repo/vendor/bin/phpcbf /usr/local/bin/phpcbf; \
 	ln -s /repo/vendor/bin/phpdoc /usr/local/bin/phpdoc; \
 	ln -s /repo/vendor/bin/jsonlint /usr/local/bin/jsonlint; \
 	ln -s /repo/vendor/bin/validate-json /usr/local/bin/validate-json; \
+	ln -s /repo/vendor/wp-cli/wp-cli/bin/wp /usr/local/bin/wp; \
 	chmod +x /usr/local/bin/* ; \
+	chmod +x /repo/vendor/wp-cli/wp-cli/php/boot-fs.php; \
 	chown -R www-data:www-data /repo;
 
 WORKDIR /project
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,75 @@
 #!/bin/bash
-/repo/vendor/bin/codecept run --report --html| tee /project/.audit/codecept.txt
-/repo/vendor/bin/phpcs --config-set installed_paths /repo/vendor/wp-coding-standards/wpcs/
-/repo/vendor/bin/phpcs --config-set error_severity 1
-/repo/vendor/bin/phpcs -i
-#/repo/vendor/bin/phpcs -nps --colors --standard=Wordpress  --report-full=/project/.audit/phpcs-full.txt --report-summary=/project/.audit/phpcs-summary.txt /project
-/repo/vendor/bin/phpcs -nps --standard=/phpcs.xml --colors /project
-# Creates php docs from project folder, creates info in .phpdocs and excludes /vendor
-/repo/vendor/bin/phpdoc -d /project -t /project/.phpdocs -i /project/vendor/
-/bin/bash
+codecept() {
+    codecept=$1
+    cmd=${2:-run}
+    echo "cmd ${cmd} 2: ${2}"
+    if [ -z "${2}" ]; then
+        echo "parameter 2 is zero"
+    fi
+    if [ "run" == "${cmd}" ] &&  [ -z "${2}" ]; then
+        echo "default test"
+        /repo/vendor/bin/codecept ${cmd} --report --html | tee /project/.audit/codecept.txt
+    else
+        # just means than a custom codecept command was passed.
+        echo "custom test"
+        /repo/vendor/bin/${@}
+    fi
+}
 
+phpcs() {
+    /repo/vendor/bin/phpcs --config-set installed_paths /repo/vendor/wp-coding-standards/wpcs/
+    /repo/vendor/bin/phpcs --config-set error_severity 1
+    /repo/vendor/bin/phpcs -i
+    /repo/vendor/bin/phpcs -nps --standard=/phpcs.xml --colors --ignore=*/.phpdocs/*,*/tests/* /project
+}
+
+phpcbf() {
+    /repo/vendor/bin/phpcbf -nps  --colors --ignore=*/.phpdocs/*,*/tests/* /project
+}
+
+phpdocs() {
+    /repo/vendor/bin/phpdoc -d /project -t /project/.phpdocs -i /project/vendor/,/project/node_modules,/project/.phpdocs
+}
+
+mysql() {
+    mysql -u root  -h bcgov_mysql -p
+}
+
+composer() {
+    $1=-"update"
+    /usr/local/bin/composer $1
+}
+
+echo "Command $1 $2"
+if [ "$1" == 'codecept' ]; then
+    codecept $@
+elif [ "$1" == 'phpcs' ]; then
+    echo "Checking php standards"
+    phpcs
+elif [ "$1" == 'phpcbf' ]; then
+    echo "Auto fixing standards"
+    phpcbf
+    phpcs
+elif [ "$1" == 'docs' ]; then
+    echo "Creating Phpdocs at .phpdocs"
+    phpdocs
+elif [ "$1" == 'bash' ]; then
+    echo "Running bash into wpbrowser"
+    /bin/bash
+elif [ "$1" == 'mysql' ]; then
+    echo "Logging into mysql client"
+    mysql
+elif [ "$1" == 'composer' ]; then
+    echo "Composer!!!"
+    composer $2
+else
+    codecept
+    phpcs
+    phpdocs
+fi
+
+
+#/repo/vendor/bin/phpcs -nps --colors --standard=Wordpress  --report-full=/project/.audit/phpcs-full.txt --report-summary=/project/.audit/phpcs-summary.txt /project
+# Creates php docs from project folder, creates info in .phpdocs and excludes /vendor
 
 #/project/../wordpress/wp-content/.env
